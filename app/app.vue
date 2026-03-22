413b627451e4326b1cc4eb3953428c8370b83ceb
George Roe <george.roe@stfc.ac.uk>
2026-03-22 12:50:53 +0000

    chore: stop website from blinding me

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,16 @@
     <NuxtPage />
   </div>
 </template>
+
+<style>
+body {
+  font-family: monospace;
+  background-color: midnightblue;
+  color: antiquewhite
+}
+
+a {
+  color: lightblue;
+  text-decoration: none;
+}
+</style>
