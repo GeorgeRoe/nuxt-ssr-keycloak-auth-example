0faa57e392578a91fa493bc8a03a8b39fb000d17
George Roe <george.roe@stfc.ac.uk>
2026-03-23 09:55:11 +0000

    fix: remove redirect from use backend api composable to stop context errors

--- frontend/app/composables/useBackendApi.ts
+++ frontend/app/composables/useBackendApi.ts
@@ -1,9 +1,4 @@
-// creates a composable for fetching data from the backend API with a base URL and error handling for unauthorized access
+// composable for making API calls to the backend server
 export const useBackendApi = createUseFetch({
     baseURL: '/api/backend',
-    async onResponseError({ response }) {
-        if (response.status === 401) {
-            await navigateTo('/api/auth/keycloak')
-        }
-    }
 })
